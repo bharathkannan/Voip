3bef377e44f616b7ff87cbee9b05e34d4a7021e9
Bharathwajan Kannan <bharath.topc@gmail.com>
2012-06-26 19:24:36 +0530

    Deleted files

--- Other projects/sipsorcery-73596/sipsorcery-core/SIPSorcery.Sys/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SIPSorcery.Sys")]
-[assembly: AssemblyDescription("Handles configuration and state information for processes.")]
-[assembly: AssemblyCopyright("Aaron Clauson")]	
-[assembly: AssemblyVersion("1.0.0.*")]
-
